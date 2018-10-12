694e469bde5c515ea61d478f2755bb9358a904ef
Vaith <vaithwee@yeah.net>
2018-10-13 00:04:59 +0800

    Add more about conditional demo

---
+++ DemoForRunoob/ConditionalStatement4.java
@@ -0,0 +1,11 @@
+public class ConditionalStatement4 {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 10;
+        if (x == 30) {
+            if (y == 10) {
+                System.out.println("x = 30 and y = 10");
+            }
+        }
+    }
+}
