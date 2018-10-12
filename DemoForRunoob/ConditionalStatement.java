d1ddc9e259b4f53c0cb8e18430cb82e6334c65a2
Vaith <vaithwee@yeah.net>
2018-10-12 17:29:24 +0800

    Add if statement demo

---
+++ DemoForRunoob/ConditionalStatement.java
@@ -0,0 +1,8 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x < 20) {
+            System.out.println("this is if statement");
+        }
+    }
+}
---
+++ DemoForRunoob/ConditionalStatement2.java
@@ -0,0 +1,10 @@
+public class ConditionalStatement2 {
+    public static void main(String[] args) {
+        int x = 30;
+        if (x < 20) {
+            System.out.println("this is if statement");
+        } else {
+            System.out.println("this is else statement");
+        }
+    }
+}
