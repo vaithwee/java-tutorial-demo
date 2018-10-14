76fd91c888d180f4f8c005e302b318733bdbc4d5
Vaith <vaithwee@yeah.net>
2018-10-14 17:17:56 +0800

    Add error catch handle demo

---
+++ DemoForRunoob/BRRead.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class BRRead {
+    public static void main(String[] args) {
+        char c;  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("input char, q is quit:");
+        do {
+            c = (char)br.read();
+            System.out.println(c);
+        } while(c != 'q');
+    }
+}
